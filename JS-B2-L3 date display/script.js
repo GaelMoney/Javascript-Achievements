9929ed940eb0bbd37772163f46cc9f4b1ee0f3c3
unknown <25393@ma-web.nl>
2021-02-12 10:50:28 +0100

    added divs to body of HTML

--- JS-B2-L3 date display/script.js
+++ JS-B2-L3 date display/script.js
@@ -1,5 +1,8 @@
 function toonDatum(){
     document.getElementById(day)
+    document.getElementById(year)
+    document.getElementById(month)
+    
 }
 
 window.addEventListener('DOMContentLoaded', toonDatum);
